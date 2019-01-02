b74d971c4c569094a6230bd4dbc4f2983f38facb
cngai <cngai1223@gmail.com>
2019-01-01 22:12:08 -0800

    added icons for sidebar

--- src/components/LeftSideBar.js
+++ src/components/LeftSideBar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import github_icon from '../images/github-logo.png';
+import linkedin_icon from '../images/linkedin-logo.png';
+import flickr_icon from '../images/flickr-logo.png';
 
 import '../styles/LeftSideBar.css';
 
@@ -7,8 +9,10 @@ class LeftSideBar extends React.Component {
 	render() {
 		return(
 			<div className="leftsb-container">
-				<div className="github-container">
+				<div className="icon-container">
+					<img className="linkedin-icon" src={linkedin_icon} />
 					<img className="github-icon" src={github_icon} />
+					<img className="flickr-icon" src={flickr_icon} />
 				</div>
 			</div>
 		);
